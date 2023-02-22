17ef9636a116228037987c310e97a334ea6012a1
unknown <egonzalezsuarez07@gmail.com>
2023-02-22 12:01:55 -0500

    Codigo terminado

--- target/classes/app.js
+++
@@ -1,9 +0,0 @@
-var color = function (){
-    titulo = document.querySelector("h1")
-    if (titulo.style.color == "blue"){
-        titulo.style.color = "red";
-    }else{
-        titulo.style.color = "blue";
-    }
-
-}
